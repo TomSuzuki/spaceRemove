5be3acb21e8d6d1ad375a675136ff2b99147d711
TomSuzuki <tomsuzuki2633@gmail.com>
2021-06-25 15:35:53 +0900

    v 1.0.1

--- main.js
+++ main.js
@@ -2,22 +2,19 @@
 function remove_space(id_input, id_output) {
     let text = document.getElementById(id_input).value;
 
-    text = replaceAll(text, "\n", "");
-    text = replaceAll(text, "\r", "");
-    text = replaceAll(text, " ", "");
+    text = replaceAll(text, ["\n", "\r", " "], "");
 
     document.getElementById(id_output).innerText = text;
 }
 
-function replaceAll(string, target, replacement) {
-    for (var i in target) {
-        var a = "";
+function replaceAll(text, targets, replacement) {
+    for (let i in targets) {
+        let temp = "";
         while (true) {
-            a = string.replace(target[i], replacement);
-            if (a == string) break;
-            string = a;
+            temp = text.replace(targets[i], replacement);
+            if (temp == text) break;
+            text = temp;
         }
-        string = a;
     }
-    return a;
+    return text;
 }
